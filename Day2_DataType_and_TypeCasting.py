512794f8ff6ce97a56dc3c02ce8e4b8be21f36cc
KrishSoni019 <krishdsoni019@gmail.com>
2025-07-22 23:22:12 +0530

    Add files via upload

---
+++ Day2_DataType_and_TypeCasting.py
@@ -0,0 +1,15 @@
+#Data Type And TypeCasting
+
+a = 2 # int 
+b = "Krish" #string
+
+c=type(a)
+d=type(b)
+
+print(c)
+print(d)
+
+e = float(a) # Chnage type of A from int to Float
+
+print(e)
+print(type(e))
